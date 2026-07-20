959f1cf2f52fdda3c8a02a0a71d7c63488b5aa16
Renat Nasyrov <renat-nasyrov@yandex.ru>
2026-07-20 20:30:23 +0200

    Write "End Of Track" meta event to meta info track as well.
    Without this, MIDI-file is considered invalid by some programs (e.g. Audacious).

--- mdx2midi.c
+++ mdx2midi.c
@@ -89,6 +89,7 @@ int main(int argc, char **argv) { // NOTE: the tempo in the output midi does not
 		char buf[PATH_MAX];
 		snprintf(buf, sizeof(buf), "Converted from %s", argv[optind]);
 		midi_track_write_text(first_track, 0, buf, -1);
+		midi_track_write_track_end(first_track, 0);
 
 		struct mdx_driver mdx_driver;
 		struct midi_timer_driver timer_driver;
